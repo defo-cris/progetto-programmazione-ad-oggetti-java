ae434e789d04de4ba892b97ac6c6a8b8ce06b5eb
simone-viozzi <viozzis333@gmail.com>
2019-07-08 17:37:02 +0200

    new class to host the data from the csv

---
+++ src/main/java/com/example/progetto/dataCsvRow.java
@@ -0,0 +1,38 @@
+package com.example.progetto;
+
+public class dataCsvRow
+{
+    int id;
+    int Nid;
+    int Original_ID;
+    String Name;
+    String Project_acronym;
+    String Visual;
+    String Project_description;
+    String Results;
+    String[] Coordinators;
+    String[] Partners;
+    String[] Project_address_es;
+    String Project_postal_code_s;
+    String Project_town_s;
+    String[] Project_country_ies;
+    float[] Project_location_latitude;
+    float[] Project_location_longitude;
+    String Link_to_a_video;
+    String Timeframe_start;
+    String Timeframe_end;
+    String Project_webpage;
+    String Related_links;
+    String EU_Budget_MFF_heading;
+    String Programme_name;
+    String Funding_area;
+    String EC_s_priorities;
+    String EU_Budget_contribution;
+    long Total_project_budget;
+    String Author;
+    String Language;
+
+
+
+
+}
